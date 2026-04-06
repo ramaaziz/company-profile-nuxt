9f05541649b2aa3e38248c81d0cebbf071cbfd83
rama aziz <rama.aziz77@gmail.com>
2026-04-06 12:07:37 +0700

    init

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+const nuxtApp = useNuxtApp();
+const { activeHeadings, updateHeadings } = useScrollspy();
+
+const items = computed(() => [
+  {
+    label: "Portfolio",
+    to: "#features",
+    active:
+      activeHeadings.value.includes("features") &&
+      !activeHeadings.value.includes("steps"),
+  },
+  {
+    label: "Sertifikasi",
+    to: "#steps",
+    active: activeHeadings.value.includes("steps"),
+  },
+  {
+    label: "Testimonials",
+    to: "#testimonials",
+    active:
+      activeHeadings.value.includes("testimonials") &&
+      !activeHeadings.value.includes("pricing"),
+  },
+]);
+
+nuxtApp.hooks.hookOnce("page:finish", () => {
+  updateHeadings(
+    [
+      document.querySelector("#features"),
+      document.querySelector("#steps"),
+      document.querySelector("#testimonials"),
+    ].filter(Boolean) as Element[],
+  );
+});
+</script>
+
+<template>
+  <UHeader>
+    <template #left>
+      <NuxtLink to="/">
+        <img src="/logos/logo-pjt.png" loading="lazy" width="32" />
+      </NuxtLink>
+      <span>PT Permata Jaya Tehnik</span>
+    </template>
+
+    <template #right>
+      <UNavigationMenu :items="items" variant="link" class="hidden lg:block" />
+
+      <UColorModeButton />
+    </template>
+
+    <template #body>
+      <UNavigationMenu :items="items" orientation="vertical" class="-mx-2.5" />
+    </template>
+  </UHeader>
+</template>
