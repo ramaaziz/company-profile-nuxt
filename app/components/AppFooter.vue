ab363b950d2b9564bb0ff1e9f8d4249f483a14cc
rama aziz <rama.aziz77@gmail.com>
2026-04-07 00:50:25 +0700

    feat: footer

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,122 +1,65 @@
-<script setup lang="ts">
-const columns = [{
-  label: 'Resources',
-  children: [{
-    label: 'Help center'
-  }, {
-    label: 'Docs'
-  }, {
-    label: 'Roadmap'
-  }, {
-    label: 'Changelog'
-  }]
-}, {
-  label: 'Features',
-  children: [{
-    label: 'Affiliates'
-  }, {
-    label: 'Portal'
-  }, {
-    label: 'Jobs'
-  }, {
-    label: 'Sponsors'
-  }]
-}, {
-  label: 'Company',
-  children: [{
-    label: 'About'
-  }, {
-    label: 'Pricing'
-  }, {
-    label: 'Careers'
-  }, {
-    label: 'Blog'
-  }]
-}]
-
-const toast = useToast()
-
-const email = ref('')
-const loading = ref(false)
-
-function onSubmit() {
-  loading.value = true
-
-  toast.add({
-    title: 'Subscribed!',
-    description: 'You\'ve been subscribed to our newsletter.'
-  })
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <USeparator
-    icon="i-simple-icons-nuxtdotjs"
-    class="h-px"
-  />
+  <USeparator />
 
   <UFooter :ui="{ top: 'border-b border-default' }">
     <template #top>
-      <UContainer>
-        <UFooterColumns :columns="columns">
-          <template #right>
-            <form @submit.prevent="onSubmit">
-              <UFormField
-                name="email"
-                label="Subscribe to our newsletter"
-                size="lg"
-              >
-                <UInput
-                  v-model="email"
-                  type="email"
-                  class="w-full"
-                  placeholder="Enter your email"
-                >
-                  <template #trailing>
-                    <UButton
-                      type="submit"
-                      size="xs"
-                      label="Subscribe"
-                    />
-                  </template>
-                </UInput>
-              </UFormField>
-            </form>
-          </template>
-        </UFooterColumns>
+      <UContainer
+        class="flex flex-col items-center gap-2 py-12 sm:flex-row sm:justify-between"
+      >
+        <div class="text-center sm:text-left">
+          <p class="text-sm leading-relaxed text-gray-600 dark:text-gray-400">
+            <span class="font-bold text-gray-900 dark:text-white text-lg"
+              >PT Permata Jaya Tehnik</span
+            >
+            <br />
+            Jl. Terusan Jakarta, Ruko Puri Dago <br />
+            Kav B No. 6 RT007 RW015 Sukamiskin <br />
+            Arcamanik, Kota Bandung - 40291 <br />
+            <span class="font-medium">022 - 63196046</span>
+          </p>
+        </div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d669.154127078746!2d107.66899595119986!3d-6.917652982295999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e78b66eecc11%3A0x6d195f5f0829301c!2sPT%20PERMATA%20JAYA%20TEHNIK%2FDAFFA%20TOURS!5e1!3m2!1sen!2sid!4v1773024067406!5m2!1sen!2sid"
+          width="100%"
+          style="border: 0"
+          loading="lazy"
+          class="rounded-xl shadow-lg w-1/3"
+        ></iframe>
       </UContainer>
     </template>
 
     <template #left>
       <p class="text-sm text-muted">
-        Built with Nuxt UI • © {{ new Date().getFullYear() }}
+        PT Permata Jaya Tehnik • © {{ new Date().getFullYear() }}
       </p>
     </template>
 
     <template #right>
-      <UButton
-        to="https://go.nuxt.com/discord"
-        target="_blank"
-        icon="i-simple-icons-discord"
-        aria-label="Nuxt on Discord"
-        color="neutral"
-        variant="ghost"
+      <UAvatar
+        src="/images/logo/logoISO9001.png"
+        alt="PT Permata Jaya Tehnik"
+        size="sm"
+        class="mr-2"
       />
-      <UButton
-        to="https://go.nuxt.com/x"
-        target="_blank"
-        icon="i-simple-icons-x"
-        aria-label="Nuxt on X"
-        color="neutral"
-        variant="ghost"
+      <UAvatar
+        src="/images/logo/logoISO14001.png"
+        alt="PT Permata Jaya Tehnik"
+        size="sm"
+        class="mr-2"
       />
-      <UButton
-        to="https://github.com/nuxt-ui-templates/landing"
-        target="_blank"
-        icon="i-simple-icons-github"
-        aria-label="Nuxt UI on GitHub"
-        color="neutral"
-        variant="ghost"
+      <UAvatar
+        src="/images/logo/logoISO45001.png"
+        alt="PT Permata Jaya Tehnik"
+        size="sm"
+        class="mr-2"
+      />
+      <UAvatar
+        src="/images/logo/smk3.png"
+        alt="PT Permata Jaya Tehnik"
+        size="sm"
+        class="mr-2"
       />
     </template>
   </UFooter>
